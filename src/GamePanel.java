dd1e386b06bd70c555397793e92fcea9550aa9c7
Sofya Dziamidava <sofya.dziamidava@gmail.com>
2021-12-08 15:37:38 +0100

    started working on game logic in GamePanel class

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,11 +1,57 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.util.Random;
 
-public class GamePanel extends JPanel implements ActionListener {
+public class GamePanel extends JPanel implements ActionListener{
 
+    static final int width = 700;
+    static final int height = 700;
+    static final int boxSize = 30;
+    static final int gameUnits = ((width*height / boxSize));
+    static final int speed = 50;
 
+    final int[][] coordinates = new int[gameUnits][gameUnits];
 
+    int snakeSize = 4;
+    String course = "Right";
+    int score = 0;
+    Timer timer;
+    Random random;
+
+    public GamePanel(){
+        setPreferredSize(new Dimension(width, height));
+        setBackground(Color.LIGHT_GRAY);
+        this.setFocusable(true);
+
+        startOfTheGame();
+    }
+
+    public void startOfTheGame(){
+
+    }
+
+    public void displayApple(){
+
+    }
+
+    public void moveSnake(){
+
+    }
+
+    public void changeDirection(){
+
+    }
+
+    public void eatApple(){
+
+    }
+
+    public void endOfTheGame(){
+
+    }
 
 
 
