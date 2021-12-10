64bf0c3d37c94b1cd730e30d803ea34badb985ee
slyohmy <simon.westlin@hotmail.com>
2021-12-10 12:51:03 +0100

    Removed unused variables & customized the start button.

--- src/StartPanel.java
+++ src/StartPanel.java
@@ -1,35 +1,30 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.IOException;
 
 public class StartPanel extends JFrame implements ActionListener {
 
-    //JLabel welcomingMessage = new JLabel("Let's play snake!");
-    JButton startButton = new JButton("Let's play snake!");
-    JPanel mainPanel = new JPanel();
-    Image image1 = ImageIO.read(new File("src/kaa.jpg"));  //TODO: se till så att bilden kan ritas och visas i fönstret
+    JButton startButton;
 
-    public StartPanel() throws IOException, InterruptedException {
+    public StartPanel() {
 
         this.setLayout(new BorderLayout());
-
         JLabel background = new JLabel(new ImageIcon("src/kaa.jpg"));
-
         background.setLayout(new BorderLayout());
         startButton = new JButton("Let's play snake!");
         background.add(startButton, BorderLayout.SOUTH);
         startButton.addActionListener(this);
+        startButton.setFont(new Font("Ink Free",Font.BOLD,35));
+        startButton.setFocusable(false);
+        startButton.setBackground(Color.WHITE);
         add(background);
         this.setVisible(true);
         this.pack();
         this.setSize(600, 600);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
@@ -49,7 +44,7 @@ public class StartPanel extends JFrame implements ActionListener {
     }
 
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         StartPanel sp = new StartPanel();
     }
 }
