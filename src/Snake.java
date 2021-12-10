0508213c353aa90a4aeb91dc2f2b4273079f6f37
rausss <wolf_rosemarie@hotmail.com>
2021-12-10 13:41:38 +0100

    created a snake

--- src/Snake.java
+++ src/Snake.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 
 public class Snake {
-    public static void main(String[] args) throws IOException {
-        GameWindow gameWindow = new GameWindow();
+
+    public Snake() {
+
+
     }
 }
